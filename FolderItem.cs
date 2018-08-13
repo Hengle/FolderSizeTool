d6da88b2c78ace90bb3350aed5356878882d9967
Slicol <slicol@qq.com>
2018-08-13 22:21:21 +0800

    first commit
    Signed-off-by: Slicol <slicol@qq.com>

---
+++ FolderItem.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FolderSizeTool
+{
+    public enum SortType
+    {
+        默认,
+        从大到小,
+        从小到大
+    }
+
+    public abstract class FileSystemItem
+    {
+        public string Icon { get; set; }
+        public  string DisplayName
+        {
+            get
+            {
+                return (this is FileItem ? "文件:" : "目录:") + m_info.Name + " (" +
+                       (size > 1024 ? (size / 1024).ToString() + "kb" : size + "b") + ")";
+            } 
+        }
+        public string Name { get { return m_info.FullName; } }
+        public List<FileSystemItem> Children { get; set; }
+
+
+        protected FileSystemInfo m_info;
+        public long size { get; protected set; }
+
+        public FileSystemItem(FileSystemInfo info)
+        {
+            m_info = info;
+        }
+
+        public abstract long CalcSize();
+
+        public virtual void Sort(SortType type)
+        {
+        }
+    }
+
+    public class FileItem : FileSystemItem
+    {
+        public FileInfo info { get { return m_info as FileInfo;} }
+
+        public FileItem(FileSystemInfo info) :base(info)
+        {
+            
+        }
+
+        public override long CalcSize()
+        {
+            size = info.Length;
+            return size;
+        }
+    }
+
+    public class DirItem : FileSystemItem
+    {
+        
+        public DirectoryInfo info { get { return m_info as DirectoryInfo; } }
+
+        public DirItem(FileSystemInfo info) : base(info)
+        {
+            Children = new List<FileSystemItem>();
+        }
+
+        public override long CalcSize()
+        {
+            size = 0;
+            Children.Clear();
+
+            var fis = info.GetFiles();
+            for (int i = 0; i < fis.Length; i++)
+            {
+                var item = new FileItem(fis[i]);
+                Children.Add(item);
+                size += item.CalcSize();
+            }
+
+            var dis = info.GetDirectories("*", SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < dis.Length; i++)
+            {
+                var item = new DirItem(dis[i]);
+                Children.Add(item);
+                size += item.CalcSize();
+            }
+
+            return size;
+        }
+
+
+        public override void Sort(SortType type)
+        {
+            if (type == SortType.从大到小)
+            {
+                Children.Sort(new Compare1());
+            }
+            else if(type == SortType.从小到大)
+            {
+                Children.Sort(new Compare2());
+            }
+
+            for (int i = 0; i < Children.Count; i++)
+            {
+                Children[i].Sort(type);
+            }
+        }
+
+
+
+        class Compare1 : IComparer<FileSystemItem>
+        {
+            public int Compare(FileSystemItem x, FileSystemItem y)
+            {
+                return (int) (y.size - x.size);
+            }
+        }
+
+        class Compare2 : IComparer<FileSystemItem>
+        {
+            public int Compare(FileSystemItem x, FileSystemItem y)
+            {
+                return (int)(x.size - y.size);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using SGF;
+using SGF.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace FolderSizeTool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DirItem m_current;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitConfig();
+            InitDebuger();
+            
+        }
+
+        public void InitDebuger()
+        {
+            Debuger.Init(AppDomain.CurrentDomain.BaseDirectory + "/Log/", new SystemColorConsole());
+            Debuger.EnableLog = AppPrefs.GetInt("EnableLog", 1) > 0;
+#if DEBUG
+            Debuger.EnableLog = true;
+#endif
+            Debuger.EnableDate = true;
+            Debuger.EnableLogVerbose = false;
+            Debuger.EnableSave = true;
+            Debuger.EnableErrorStack = false;
+            Debuger.LogWarning("AppDomain:{0}", AppDomain.CurrentDomain.FriendlyName);
+            Debuger.LogWarning("WorkDir:{0}", Directory.GetCurrentDirectory());
+        }
+
+        public void InitConfig()
+        {
+            AppPrefs.Init("./AppConfig.json");
+            cbBaseDir.Text = AppPrefs.GetString("cbBaseDir","");
+
+            List<SortType> listTypes = new List<SortType>();
+            listTypes.Add(SortType.从大到小);
+            listTypes.Add(SortType.从小到大);
+
+            cbSortType.ItemsSource = listTypes;
+
+            cbSortType.SelectedIndex = AppPrefs.GetInt("cbSortType", 0);
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            var dirPath = cbBaseDir.Text;
+            if (!Directory.Exists(dirPath))
+            {
+                Debuger.LogError("目录不存在:{0}", dirPath);
+                return;
+            }
+            
+            AppPrefs.SetString("cbBaseDir", cbBaseDir.Text);
+            AppPrefs.Save();
+
+            m_current = new DirItem(new DirectoryInfo(dirPath));
+            m_current.CalcSize();
+            m_current.Sort((SortType)cbSortType.SelectedValue);
+
+            List<DirItem> list = new List<DirItem>();
+            list.Add(m_current);
+            tvDirTree.ItemsSource = list;
+        }
+
+        private void TvDirTree_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var item = tvDirTree.SelectedItem as FileSystemItem;
+            lbDirChildren.ItemsSource = item.Children;
+        }
+
+        private void cbSortType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AppPrefs.SetInt("cbSortType", cbSortType.SelectedIndex);
+            if (m_current != null)
+            {
+                m_current.Sort((SortType)cbSortType.SelectedValue);
+                List<DirItem> list = new List<DirItem>();
+                list.Add(m_current);
+                tvDirTree.ItemsSource = list;
+            }
+        }
+    }
+}
